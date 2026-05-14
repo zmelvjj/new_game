8d882823cc3fb605c7ef1309efba25a32b3b8b28
zmelvjj <zmelvjj@gmail.com>
2026-05-14 19:48:56 +0900

    Update

--- Assets/Script/move.cs
+++ Assets/Script/move.cs
@@ -3,30 +3,29 @@ using UnityEngine.InputSystem;
 
 public class move : MonoBehaviour
 {
-
     public GameObject player;
+
+    private PlayerInput playerInput;
+
     void Start()
     {
         player = this.gameObject;
+        playerInput = this.GetComponent<PlayerInput>();
+
+        playerInput.actions["Move"].performed += OnMove;
     }
 
-    void Update()
+    public void OnMove(InputAction.CallbackContext context)
     {
-        if (InputSystem.GetDevice<Keyboard>().wKey.isPressed)
-        {
-            player.transform.Translate(Vector3.forward * Time.deltaTime * 5);
+        if (context.performed){
+            Vector2 inputVector = context.ReadValue<Vector2>();
+            Vector2 movement = new Vector2(inputVector.x, inputVector.y);
+            player.transform.Translate(movement * Time.deltaTime * 5f);
         }
-        if (InputSystem.GetDevice<Keyboard>().sKey.isPressed)
-        {
-            player.transform.Translate(Vector3.back * Time.deltaTime * 5);
-        }
-        if (InputSystem.GetDevice<Keyboard>().aKey.isPressed)
-        {
-            player.transform.Translate(Vector3.left * Time.deltaTime * 5);
-        }
-        if (InputSystem.GetDevice<Keyboard>().dKey.isPressed)
-        {
-            player.transform.Translate(Vector3.right * Time.deltaTime * 5);
-        }
+    }
+
+    void OnDestroy()
+    {
+        playerInput.actions["Move"].performed -= OnMove;
     }
 }
